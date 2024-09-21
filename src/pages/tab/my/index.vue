c89431b9f7208f34e979df67003b32f9fd88704a
meme007 <meme007@gmail.com>
2024-09-21 19:38:10 +0800

    feat: 新增VIP/我的页面布局信息

--- src/pages/tab/my/index.vue
+++ src/pages/tab/my/index.vue
@@ -1,11 +1,104 @@
 <template>
-	<view>
-    My
-	</view>
+  <view style="width: 100%;height: 100vh;background-color: #101012;display: flex;flex-direction: column;">
+    <view style="width:100%; height: 200px;
+    background-image: url(/static/images/my/my_bg.webp);background-size: 100% 100%;">
+      <view style="display: flex;height: 60px;padding-right: 20px;justify-content: flex-end;"><up-icon name="scan"
+          color="#AAA" size="30"></up-icon></view>
+      <view
+        style="display: flex;margin-left: 20px;margin-right: 20px;margin-top: 20px; justify-content: space-between;align-items: flex-end;">
+        <view style="display: flex;flex-direction: row;align-items: center;">
+          <up-icon name="/static/images/my/avatar.webp" color="#2979ff" size="80"></up-icon>
+          <view style="margin-left: 10px;display: flex;flex-direction: row;align-items: center;color: #FFF;">
+            <span v-if="loginInfo===null" style="font-weight: bold;">登录/注册</span>
+            <span v-if="loginInfo!==null" style="font-size: 24px;">{{loginInfo.nickanme}}</span>
+            <up-icon v-if="loginInfo!==null" :name="getSix()" :color="getSixColor()" size="30"></up-icon>
+          </view>
+        </view>
+        <view><up-image src="/static/images/my/my_float_sign.png" width="66px" height="48px"></up-image></view>
+      </view>
+    </view>
+
+    <!-- 关注 -->
+    <view
+      style="display: flex;flex-direction: row;justify-content: space-between;margin-left: 20px;margin-right: 20px;">
+      <view style="display: flex;flex-direction: column;align-items: center;width: 80px;"
+        v-for="(item,ix) in titleNames" :key="ix">
+        <span style="color: #CCC;">-</span>
+        <view style="margin-top: 2px;color: #AAA;font-size: 14px;">{{item}}</view>
+      </view>
+    </view>
+
+    <!-- VIP广告 -->
+    <view style="display: flex;flex-direction: row;height: 60px;margin:20px 20px;
+    background:linear-gradient(to right,#4582e8,#8b3bfd);border-radius: 4px;
+    align-items: center;justify-content: center;color:#FFF;font-weight: bold;">
+      <span>开通VIP,享高清片源等9大特权</span>
+    </view>
+
+    <!-- 离线视频 -->
+    <view
+      style="display: flex;flex-direction: row;justify-content: space-between;margin-left: 20px;margin-right: 20px;">
+      <view style="display: flex;flex-direction: column;align-items: center;" v-for="(item,ix) in iconNames" :key="ix">
+        <up-icon :name="getIconUrl(0,ix)" color="#2979ff" size="30"></up-icon>
+        <view style="margin-top: 5px;color: #AAA;">{{item}}</view>
+      </view>
+    </view>
+
+    <!-- 个人服务 -->
+    <view style="margin: 20px;">
+      <view style="font-weight: bold;font-size: 24px;color:#FFF;">个人服务</view>
+      <template v-for="(subs,index) in serviceNames" :key="index">
+        <view
+          style="display: flex;flex-direction: row;justify-content: space-between;margin:15px 20px;font-size: 16px;color:#ccc;">
+          <view style="display: flex;flex-direction: column;align-items: center;" v-for="(item,ix) in subs" :key="ix">
+            <up-icon :name="getIconUrl(index+1,ix)" color="#2979ff" size="30"></up-icon>
+            <view style="margin-top: 5px;">{{item}}</view>
+          </view>
+        </view>
+      </template>
+
+    </view>
+  </view>
 </template>
 
 <script setup>
+  import {
+    ref
+  } from 'vue';
 
+  // {
+  //   nickanme: "meme007",
+  //   six: 0,
+  // }
+  const loginInfo = ref(null);
+  const titleNames = ref(["关注", "粉丝", "等级", "金币"]);
+  const iconNames = ref(["离线视频", "历史记录", "我的收藏", "消息"]);
+  const serviceNames = ref([
+    ["赚金币", "广告中心", "VIP开通记录", "大V认证"],
+    ["求片", "帮助中心", "联系客服", "设置"]
+  ])
+
+  const getSixColor = () => {
+    if (loginInfo.value) {
+      let six = loginInfo.value.six;
+      return six == 1 ? "#2979ff" : six == 3 ? "#2979ff" : "#ca2716";
+    }
+    return "#2979ff";
+  }
+
+  const getSix = () => {
+    if (loginInfo.value) {
+      let six = loginInfo.value.six;
+      return six == 1 ? "man" : six == 3 ? "backspace" : "woman";
+    }
+    return "backspace";
+  }
+
+  const getIconUrl = (row, col) => {
+    let c = row * 4 + col;
+    let url = '/static/images/my/my_icon_' + c + '.png';
+    return url;
+  }
 </script>
 
 <style>
--- src/pages/tab/vip/index.vue
+++ src/pages/tab/vip/index.vue
@@ -1,11 +1,101 @@
 <template>
-	<view>
-		Vip Index
-	</view>
+  <view style="width: 100%;background-color: #101012;display: flex;flex-direction: column;">
+
+
+
+    <!-- 此处为了让reload时不自动滚动到顶部，需要设置auto-clean-list-when-reload和auto-scroll-to-top-when-reload为false，即在reload时关闭自动清空数组和自动滚动到顶部 -->
+    <z-paging ref="paging" v-model="dataList" :loading-more-enabled="false" :auto-scroll-to-top-when-reload="false"
+      @query="queryList">
+
+      <view style="display: flex;flex-direction: column;margin: 30px 20px;">
+        <view
+          style="background-image: url(/static/images/vip/userinfo_bg.png);background-size: 100% 100%;height: 70px;">
+          <view
+            style="margin: 0px 25px;height: 50px;display: flex;flex-direction: row;align-items: center;justify-content: space-between;">
+            <view style="display: flex;flex-direction: row;">
+              <up-icon name="/static/images/my/avatar.webp" size="30px"></up-icon>
+              <view style="display: flex;flex-direction: column;margin-left: 8px;align-items: flex-start;">
+                <up-icon name="level" labelColor="#FFF" space="5px" :bold="true" label="007" labelPos="left"></up-icon>
+                <span style="color: #CCC;font-size: 12px;margin-top: 3px;">你还没有开通VIP</span>
+              </view>
+            </view>
+            <view
+              style="color:#972d19;background: linear-gradient(to bottom,#fee8c2,#fec2a2);padding: 5px 10px;border-radius: 3px;font-weight: bold;">
+              开通VIP</view>
+          </view>
+        </view>
+
+        <!-- INFO -->
+        <view
+          style="padding-top: 15px; display: flex;flex-direction: column;background: linear-gradient(to bottom,#feeac4,#fec2a2);border-radius: 0px 0px 10px 10px;">
+          <up-image :show-loading="true" src="/static/images/vip/vip_title.webp" width="100%" height="30px"></up-image>
+          <!-- Grid -->
+          <view style="padding-bottom: 40px;">
+            <template v-for="(subs,index) in titleNames" :key="index">
+              <view
+                style="margin-top: 40px; width: 100%; display: flex;flex-direction: row;justify-content: space-evenly;">
+                <view style="display: flex;flex-direction: column;align-items: center;width: 100px;"
+                  v-for="(item,ix) in subs" :key="ix">
+                  <up-image :show-loading="true" :src="getIconUrl(index,ix)" width="50px" height="30px"></up-image>
+                  <view style="margin-top: 10px;color: #444244;font-size: 12px;">{{item}}</view>
+                </view>
+              </view>
+            </template>
+          </view>
+        </view>
+
+        <!-- List -->
+        <view style="display: flex;flex-direction: column;">
+          <view v-for="(item,ix) in listInfos" :key="ix"
+            style="display: flex;flex-direction: row;width: 100%;height: 60px;justify-content: space-between;">
+            <view style="display: flex;width: 100%;flex-direction: row;color:#EEE;align-items: center;font-size: 18px;">
+              <up-image :show-loading="true" :src="getSubIconUrl(ix)" width="24px" height="24px"></up-image>
+              <span style="margin-left: 10px;">{{item}}</span>
+            </view>
+            <up-icon name="arrow-right"></up-icon>
+          </view>
+
+        </view>
+
+      </view>
+
+      <up-gap height="40" bgColor="transparent"></up-gap>
+    </z-paging>
+
+
+  </view>
 </template>
 
 <script setup>
+  import {
+    ref
+  } from "vue";
+  const dataList = ref([])
+  const titleNames = ref([
+    ["超清4K", "过滤播放器广告", "求片"],
+    ["云加速", "倍速播放", "求片"],
+    ["自定义弹幕", "VIP表情包", "发起投票权"]
+  ]);
 
+  const listInfos = ref(["会员代充", "会员兑换", "常见问题", "VIP会员服务条款"]);
+
+  const getIconUrl = (row, col) => {
+    let c = row * 3 + col;
+    let url = '/static/images/vip/vip_' + c + '.png';
+    return url;
+  }
+  const getSubIconUrl = (ix) => {
+    let url = '/static/images/vip/vip_icon_' + ix + '.png';
+    return url;
+  }
+
+  // @query所绑定的方法不要自己调用！！需要刷新列表数据时，只需要调用paging.value.reload()即可
+  const queryList = (pageNo, pageSize) => {
+    // 组件加载时会自动触发此方法，因此默认页面加载时会自动触发，无需手动调用
+    // 这里的pageNo和pageSize会自动计算好，直接传给服务器即可
+    // 模拟请求服务器获取分页数据，请替换成自己的网络请求
+    //paging.value.complete([21, 3, 3, 3, 3, 4]);
+  }
 </script>
 
 <style>
